bc3e161cf0ebcd3b0fe4b9c73c9a827dc64dd6b2
Lukas <lukas.schiermayer@gmail.com>
2017-10-04 10:11:14 +0200

    Moodle testfiles added

--- server.cpp
+++ server.cpp
@@ -6,12 +6,24 @@ using namespace std;
 
 int main()
 {
-	int x = 10;
+	int x = 5;
+	cout << "\033[2J\033[1;1H";
 
 	while(x)
 	{
-		cout << "Herbert" << endl;
+		cout << "Herbert";
+		cout.flush();
 		this_thread::sleep_for(chrono::milliseconds(750));
+		cout << ".";
+		cout.flush();
+		this_thread::sleep_for(chrono::milliseconds(750));
+		cout << ".";
+		cout.flush();
+		this_thread::sleep_for(chrono::milliseconds(750));
+		cout << ".";
+		cout.flush();
+		this_thread::sleep_for(chrono::milliseconds(750));
+		cout << "\033[2J\033[1;1H";
 		--x;
 	}
 	cout << "WTF IS THIS SHIT?!" << endl;
